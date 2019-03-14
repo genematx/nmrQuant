81e6c795885ba65e050623c6f1415aa5c67df1b2
genematx <yma80@canterbury.ac.nz>
2019-03-14 13:15:20 +1300

    version change

--- main.py
+++ main.py
@@ -34,13 +34,8 @@ try:
 except ImportError:
     figureoptions = None
 
-<<<<<<< HEAD
-version = '0.17.0'
+version = '0.18.0'
 compile_standalone = True   # Change to False for debugging/development to output the results into the usual console
-=======
-version = '0.16.0'
-compile_standalone = False   # Change to False for debugging/development to output the results into the usual console
->>>>>>> origin/master
 
 cursord = {
     cursors.MOVE: Qt.SizeAllCursor,
