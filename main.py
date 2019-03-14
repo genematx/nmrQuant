a4aac0caf9eea5917e5182b9363827363f5ff407
genematx <yma80@canterbury.ac.nz>
2019-03-14 13:12:34 +1300

    Version change

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ try:
 except ImportError:
     figureoptions = None
 
-version = '0.16.0'
+version = '0.17.0'
 compile_standalone = True   # Change to False for debugging/development to output the results into the usual console
 
 cursord = {
