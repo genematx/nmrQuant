efc6083cb618bbf01e39800f9e186c2cc71f6c94
genematx <eugene.dn@gmail.com>
2019-03-22 11:19:28 +1300

    fix chemDB

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ try:
 except ImportError:
     figureoptions = None
 
-version = '0.18.4'
+version = '0.18.5'
 compile_standalone = False   # Change to False for debugging/development to output the results into the usual console
 
 cursord = {
