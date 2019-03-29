24ce81c7179fc0f1e5fd87cd4be5b86101d990d2
genematx <eugene.dn@gmail.com>
2019-03-29 14:10:36 +1300

    Added wden function

--- MainLogic.py
+++ MainLogic.py
@@ -8,6 +8,7 @@ from leastsquares import *
 import sys
 import tabulate
 from math import ceil
+import pywt
 
 # Functions for generating FIDs and optimization
 from scipy import optimize
@@ -2976,3 +2977,98 @@ def updateFromFlat(hierDict, flatKeys, flatVals):
 def next_pow_of_2(x):
     # Returns the smallest power of 2 greater than x
     return 0 if x == 0 else 2**(x - 1).bit_length()
+
+def wden(x):
+    """Wavelet denoising of a 1D signal."""
+    y = x
+    return y
+    """
+
+        for k = 1:n
+        flk = first(k):last(k);
+        if strcmp(tptr,'sqtwolog') || strcmp(tptr,'minimaxi')
+            thr = thselect(c,tptr);
+        else
+            if s(k) < sqrt(eps) * max(c(flk))
+                thr = 0;
+            else
+                thr = thselect(c(flk)/s(k),tptr);
+            end
+        end                                     % threshold.
+        thrs(k)      = thr * s(k);                  % rescaled threshold.
+        cxd(flk) = wthresh(c(flk),sorh,thrs(k));    % thresholding or shrinking.
+        end
+
+
+
+    function thr = thselect(x,tptr)
+    %THSELECT Threshold selection for de-noising.
+    %   THR = THSELECT(X,TPTR) returns threshold X-adapted value
+    %   using selection rule defined by string TPTR.
+    %
+    %   Available selection rules are:
+    %   TPTR = 'rigrsure', adaptive threshold selection using
+    %       principle of Stein's Unbiased Risk Estimate.
+    %   TPTR = 'heursure', heuristic variant of the first option.
+    %   TPTR = 'sqtwolog', threshold is sqrt(2*log(length(X))).
+    %   TPTR = 'minimaxi', minimax thresholding.
+    %
+    %   Threshold selection rules are based on the underlying
+    %   model y = f(t) + e where e is a white noise N(0,1).
+    %   Dealing with unscaled or nonwhite noise can be handled
+    %   using rescaling output threshold THR (see SCAL parameter
+    %   in WDEN).
+    %
+    %   See also WDEN.
+
+    %   M. Misiti, Y. Misiti, G. Oppenheim, J.M. Poggi 12-Mar-96.
+    %   Last Revision: 20-Dec-2010.
+    %   Copyright 1995-2010 The MathWorks, Inc.
+
+    validateattributes(x, {'numeric'}, {'2d'}, 'THSELECT', 'X')
+
+    if isrow(x)
+        x = x(:);
+    end
+    [n,m] = size(x);
+
+    switch tptr
+        case 'rigrsure'
+            sx = sort(abs(x),1);
+            sx2 = sx.^2;
+            N1 = repmat((n-2*(1:n))',1,m);
+            N2 = repmat((n-1:-1:0)',1,m);
+            CS1 = cumsum(sx2,1);
+            risks = (N1+CS1+N2.*sx2)./n;
+            [~,best] = min(risks,[],1);
+            % thr will be row vector
+            thr = sx(best);
+
+        case 'heursure'
+            %%
+            hthr = (2*log(n)).^0.5;
+            eta = sum(abs(x).^2-n,1)./n;
+            crit = (log(n)/log(2))^(1.5)/(n.^0.5);
+            thr = thselect(x,'rigrsure');
+            thr(thr > hthr) = hthr;
+            thr(eta < crit) = hthr;
+
+        case 'sqtwolog'
+            thr = (2*log(n)).^0.5;
+            thr = repelem(thr,size(x,2));
+
+        case 'minimaxi'
+            if n <= 32
+                thr = repelem(0, m);
+            else
+                t = 0.3936 + 0.1829*(log(n)/log(2));
+                thr = repelem(t,m);
+            end
+
+        otherwise
+            error(message('Wavelet:FunctionArgVal:Invalid_ArgVal'))
+    end
+
+
+
+    """
