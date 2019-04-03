c9c4c1459ad26372998328dd28b033a9484e8c95
Eugene <eugene.dn@gmail.com>
2019-04-03 17:03:50 +1300

    Working branch (#13)
    * labels
    
    * gitignore
    
    * fixed optimization routine
    
    * fixes
    
    * fix errors
    
    * add fid folder to gitignore
    
    * Added wden function
    
    * Assignment operators
    
    Make the assignment operators compatible with a newer version of numpy
    
    * Fix fitting J-couplings

--- MainLogic.py
+++ MainLogic.py
@@ -8,6 +8,7 @@ from leastsquares import *
 import sys
 import tabulate
 from math import ceil
+import pywt
 
 # Functions for generating FIDs and optimization
 from scipy import optimize
@@ -1894,6 +1895,7 @@ class Datum():
 
             # Define modelled and measured signals
             Z, y = np.hstack((zFinRange, bslnPoly)), yFinRange
+
         ns, nz = Z.shape     # Number of samples and (model signals + baselines)
         na = len(repRootNames)    # Number of model signals, and hence the resulting amplitudes
         if np.isnan(Z).any() or np.isinf(Z).any():             # This can happen if some chemical shifts are set to None
@@ -1932,7 +1934,9 @@ class Datum():
         if not useComplex:
             Z, y = Z.real, (y*np.exp(-1j*theta)).real
         else:
-            Z, y = np.vstack([Z.real, Z.imag]), np.vstack([(y*np.exp(-1j*theta)).real, (y*np.exp(-1j*theta)).imag])
+            pass
+            y = y*np.exp(-1j*theta)
+            #Z, y = np.vstack([Z.real, Z.imag]), np.vstack([(y*np.exp(-1j*theta)).real, (y*np.exp(-1j*theta)).imag])
 
         # 3.3. Variance of noise
         key = ('.', 'sigma2', 0)
@@ -1959,25 +1963,27 @@ class Datum():
             Gz = 1.0*np.abs(Z)
 
         Gz[:, na:] = 0
-        #self.ZZZ, self.yyy = Z, y
         result, ampl, sigma2, meta = log_likelihood(Z, y, ampl=ampl, sigma2=sigma2, \
             Gz=Gz, Gy=None, gamma=gamma, m0=m0, iS0=iS0, a_sigma2=a_sigma2, b_sigma2=b_sigma2, \
             funcType=funcType, robust=robust)
-        diff_theta = np.asscalar( 1/2*np.angle(ampl[:na].T.dot(ampl[:na])) )   # Global phase
+        diff_theta = np.asscalar( 1/2*np.angle(ampl[:na].T.dot(ampl[:na])) )   # Global phase estimated from the complex valued amplitudes
         theta = (theta + diff_theta + np.pi) % (2 * np.pi) - np.pi
         m_ampl = ampl*np.exp(-1j*diff_theta)
-        m_ampl[:na] = m_ampl[:na].real
-        if m_ampl[:na].sum() < 0:      # Make sure that all amplitudes are positive
+        #m_ampl[:na] = m_ampl[:na].real
+        if m_ampl[:na].real.sum() < 0:      # Make sure that all amplitudes are positive
             m_ampl = - m_ampl
-            theta = (theta + +np.pi + np.pi) % (2 * np.pi) - np.pi
+            theta = (theta + np.pi + np.pi) % (2 * np.pi) - np.pi
             if not useComplex: evalParsH['.']['theta'][0] = (evalParsH['.']['theta'][0] + np.pi + np.pi) % (2 * np.pi) - np.pi  # Always update the phase if it needs to be flipped
+        m_ampl[:na] = np.abs(m_ampl[:na])
         gamma = meta['gamma']
         S_ampl = meta['ampl'][1]
         a_sigma2, b_sigma2 = meta['sigma2']
+        #print(m_ampl)
 
         mult = 1   # sum(m_ampl)     # Multiplier (can be used to output normalized amplitudes)
-        for lbl, val in zip(self.repRootNames, np.abs(m_ampl[:na])):
-            evalParsH[lbl]['ampl'][0] = np.asscalar(val) / mult
+        for lbl, val in zip(self.repRootNames, m_ampl[:na]):
+            evalParsH[lbl]['ampl'][0] = np.asscalar(np.abs(val)) / mult
+            evalParsH[lbl]['phase'][0] = np.asscalar(np.angle(val))
         evalParsH['.']['mult'][0] = mult
         evalParsH['.']['theta'][0] = theta            # Update the phase
         evalParsH['.']['sigma2'][0] = sigma2
@@ -2976,3 +2982,98 @@ def updateFromFlat(hierDict, flatKeys, flatVals):
 def next_pow_of_2(x):
     # Returns the smallest power of 2 greater than x
     return 0 if x == 0 else 2**(x - 1).bit_length()
+
+def wden(x):
+    """Wavelet denoising of a 1D signal."""
+    y = x
+    return y
+    """
+
+        for k = 1:n
+        flk = first(k):last(k);
+        if strcmp(tptr,'sqtwolog') || strcmp(tptr,'minimaxi')
+            thr = thselect(c,tptr);
+        else
+            if s(k) < sqrt(eps) * max(c(flk))
+                thr = 0;
+            else
+                thr = thselect(c(flk)/s(k),tptr);
+            end
+        end                                     % threshold.
+        thrs(k)      = thr * s(k);                  % rescaled threshold.
+        cxd(flk) = wthresh(c(flk),sorh,thrs(k));    % thresholding or shrinking.
+        end
+
+
+
+    function thr = thselect(x,tptr)
+    %THSELECT Threshold selection for de-noising.
+    %   THR = THSELECT(X,TPTR) returns threshold X-adapted value
+    %   using selection rule defined by string TPTR.
+    %
+    %   Available selection rules are:
+    %   TPTR = 'rigrsure', adaptive threshold selection using
+    %       principle of Stein's Unbiased Risk Estimate.
+    %   TPTR = 'heursure', heuristic variant of the first option.
+    %   TPTR = 'sqtwolog', threshold is sqrt(2*log(length(X))).
+    %   TPTR = 'minimaxi', minimax thresholding.
+    %
+    %   Threshold selection rules are based on the underlying
+    %   model y = f(t) + e where e is a white noise N(0,1).
+    %   Dealing with unscaled or nonwhite noise can be handled
+    %   using rescaling output threshold THR (see SCAL parameter
+    %   in WDEN).
+    %
+    %   See also WDEN.
+
+    %   M. Misiti, Y. Misiti, G. Oppenheim, J.M. Poggi 12-Mar-96.
+    %   Last Revision: 20-Dec-2010.
+    %   Copyright 1995-2010 The MathWorks, Inc.
+
+    validateattributes(x, {'numeric'}, {'2d'}, 'THSELECT', 'X')
+
+    if isrow(x)
+        x = x(:);
+    end
+    [n,m] = size(x);
+
+    switch tptr
+        case 'rigrsure'
+            sx = sort(abs(x),1);
+            sx2 = sx.^2;
+            N1 = repmat((n-2*(1:n))',1,m);
+            N2 = repmat((n-1:-1:0)',1,m);
+            CS1 = cumsum(sx2,1);
+            risks = (N1+CS1+N2.*sx2)./n;
+            [~,best] = min(risks,[],1);
+            % thr will be row vector
+            thr = sx(best);
+
+        case 'heursure'
+            %%
+            hthr = (2*log(n)).^0.5;
+            eta = sum(abs(x).^2-n,1)./n;
+            crit = (log(n)/log(2))^(1.5)/(n.^0.5);
+            thr = thselect(x,'rigrsure');
+            thr(thr > hthr) = hthr;
+            thr(eta < crit) = hthr;
+
+        case 'sqtwolog'
+            thr = (2*log(n)).^0.5;
+            thr = repelem(thr,size(x,2));
+
+        case 'minimaxi'
+            if n <= 32
+                thr = repelem(0, m);
+            else
+                t = 0.3936 + 0.1829*(log(n)/log(2));
+                thr = repelem(t,m);
+            end
+
+        otherwise
+            error(message('Wavelet:FunctionArgVal:Invalid_ArgVal'))
+    end
+
+
+
+    """
